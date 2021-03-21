0ea7f92f84adac767d8953e0e7b02bf96990f283
OshriYoktan <oshriyok1@gmail.com>
2021-03-21 17:06:43 +0200

    oshris

--- frontend/src/views/explore.vue
+++ frontend/src/views/explore.vue
@@ -27,5 +27,6 @@ export default {
   components: {
     stationList
   },
+
 };
 </script>
--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -8,6 +8,7 @@
 <script>
 export default {
   name: "Home",
-  components: {},
+  components: {
+  },
 };
 </script>
