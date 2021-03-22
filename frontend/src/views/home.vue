379bbdb333fe848b4002ced641e05f04c5e3854a
OshriYoktan <oshriyok1@gmail.com>
2021-03-22 12:35:55 +0200

    oshri added imgs

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home-page column-layout-container">
-    <h1>Home</h1>
-    <h1>a big HERO layout</h1>
-    <router-link class="link" to="/explore">start listening </router-link>
+    <div class="hero-container">
+      <h1>Home</h1>
+      <h1>a big HERO layout</h1>
+      <router-link class="link" to="/explore">start listening </router-link>
+    </div>
     <div class="explore-sub-container column-layout-container">
       <div class="explore-header column-layout-container">
         <h3>Start listening to the best stations</h3>
