04b85f3e004341385ad008479ba2e05e1bbe807e
OshriYoktan <oshriyok1@gmail.com>
2022-12-06 18:19:04 +0200

    first commit

---
+++ backend/public/precache-manifest.a3bc8de4ed6cb604f26cefdd5ab99ce8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e553937dc9ab17e4c58",
+    "url": "/css/app.6ac847ca.css"
+  },
+  {
+    "revision": "a212e21cb501293f2f0b",
+    "url": "/css/chunk-vendors.c470e980.css"
+  },
+  {
+    "revision": "2e981d24acae70b64ca4bd957c21819b",
+    "url": "/fonts/ReemKufi-Regular.2e981d24.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3bd5943ea33003163abf96d5746d07f1",
+    "url": "/img/no-img-jukify.3bd5943e.jpg"
+  },
+  {
+    "revision": "23fbb83a8fa549998a947d4bbe2d6ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e553937dc9ab17e4c58",
+    "url": "/js/app.ef8f8088.js"
+  },
+  {
+    "revision": "a212e21cb501293f2f0b",
+    "url": "/js/chunk-vendors.782db87c.js"
+  },
+  {
+    "revision": "ce06d44c5486617436c28682ba54b91a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
